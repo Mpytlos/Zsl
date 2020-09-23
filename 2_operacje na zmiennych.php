5c6dcf62999d2c0339b69eaeeb8ccf3885178d90
Mpytlos <mpytlos.mp@gmail.com>
2020-09-23 21:48:20 +0200

    uzupełnienie notatki

--- 2_operacje na zmiennych.php
+++ 2_operacje na zmiennych.php
@@ -1,27 +1,76 @@
 <?php
-//wersja php
-echo PHP_VERSION.'<br>';
-//echo phpinfo();
-$pow = 2 ** 8;
-echo $pow.'<br>';  //256
+  //wersja PHP 7.4.9
+  echo PHP_VERSION,"<hr>";
+  //echo phpinfo();
 
-echo 'a'. 'b'.'c'; //a+b+c=abc
-echo '<br>';
-echo 'a','b','c'; //abc
+  //echo 'a','b','c'; - szybsza, abc
+  //echo 'a'.'b'.'c'; - wolniejsza, a+b+c->abc
 
-//operatory bitowe
-//and &, or |, xor ^, not ~, <<,>>
-$x = 0b1010;
-echo "<hr>$x<br>";
-$x = $x << 2; //101000(2) = 8+32=40 dodaje dwa 0
-echo "$x<br>";
-$x = $x >> 1; //10100(2) = 4+16=20 dodaje jedno 0
-echo "$x<br>";
+  $potega = 2 ** 10;
+  echo "$potega","<hr>"; //1024
 
-//porównanie <=>
-$x = 10;
-$y = 20;
-echo $x <=> $y;
-$result = $x <=> $y;
-echo $result;
+  //operatory bitowe
+  // and &, or |, not ~, xor ^, <<, >>
+
+  $x = 0b1010;
+  echo "$x<br>"; //10
+
+  $x = $x << 1;
+  echo "$x<br>"; // 10100 => 20
+
+  $x = $x >> 3;
+  echo "$x<hr>"; // 10 => 2
+
+  //porównanie
+
+  $a = 10;
+  $b = 5;
+
+  echo "a = 10, b = 5 <br><br>";
+
+  echo "Czy a równa się b? => ";
+  var_dump($a == $b);
+  echo "<br>";
+
+  echo "Czy a jest identyczna rodzajem i wartością do b? => ";
+  var_dump($a === $b);
+  echo "<br>";
+
+  echo "Czy a różna od b? => ";
+  var_dump($a != $b);
+  echo "<br>";
+
+  echo "Czy a nie jest identyczna rodzajem i wartością do b? => ";
+  var_dump($a <> $b);
+  echo "<br>";
+
+  echo "Czy a jest mniejsza od b? => ";
+  var_dump($a < $b);
+  echo "<br>";
+
+  echo "Czy a jest większa od b? => ";
+  var_dump($a > $b);
+  echo "<br>";
+
+  echo "Czy a jest miejsza lub równa się b? => ";
+  var_dump($a <= $b);
+  echo "<br>";
+
+  echo "Czy a jest większa lub równa się b? => ";
+  var_dump($a >= $b);
+  echo "<hr>";
+
+  //porównanie v2
+
+  $x = -10;
+  $y = 1;
+  echo $x <=> $y;
+  echo "<br>";
+
+  $result = $x <=> $y;
+  echo $result;
+  //jeżeli jest mniejsza, ma wartość -1
+  //jeżeli jest równa, ma wartość 0
+  //jeżeli jest większa, ma wartość 1
+
  ?>
